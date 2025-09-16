84af3dd142bbb02284f8c16fdb4c42bd0340c831
RBISUK <rbr.01@outlook.com>
2025-09-16 15:22:02 +0000

    fix: restore Tailwind CSS build for production

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/**/*.html"
   ],
   theme: {
     extend: {},
