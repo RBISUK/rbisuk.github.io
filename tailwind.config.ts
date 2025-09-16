1a0a786701683710972e12539a79570b35c18d7a
RBISUK <rbr.01@outlook.com>
2025-09-16 15:11:16 +0000

    fix: restore Tailwind CSS purge paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,13 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 export default config;
